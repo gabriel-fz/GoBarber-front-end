49494fc5f65bbf01f712fef18e768c50ccb1480a
gabriel-fz <gabrielfzancanela@gmail.com>
2020-03-06 13:30:47 -0300

    Cadastro e autorizaçao de usuarios concluido

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -7,8 +7,15 @@ export function signInRequest(email, password) {
 
 export function signInSuccess(token, user) {
   return {
-    type: '@auth/SIGN_IN_SUCESS',
+    type: '@auth/SIGN_IN_SUCCESS',
     payload: { token, user },
+  };
+}
+
+export function signUpRequest(name, email, password) {
+  return {
+    type: '@auth/SIGN_UP_REQUEST',
+    payload: { name, email, password },
   };
 }
 
